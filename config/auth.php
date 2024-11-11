515ba684ffffc8df714153746d0f4f35caa3c76e
Mishal12g <Mishal12g@gamil.com>
2024-11-11 17:25:32 +0300

    setting api

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,12 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+
+        'api' => [
+            'driver' => 'sanctum', // или 'token' в зависимости от твоей конфигурации
+            'provider' => 'users',
+            'hash' => false,
+        ],
     ],
 
     /*
